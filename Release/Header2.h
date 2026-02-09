e4c7d867165f1c464b7661c07bf609fbfe345646
Mushu1233 <durnevdaniil921@gmail.com>
2026-02-09 10:12:21 +0300

    Add files via upload

---
+++ Release/Header2.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __Mushu__
+#define __Mushu__
+
+//library
+#include<cstdio>
+
+//Macros
+//+
+#defaine MUL_1 0xAAf52015
+#defaine MUL_5 0xAAf54f15
+#defaine MUL_12 0xAAf52125
+#defaine MUL_16 0xAAf52014
++//
+
+
+
+
+init (MUL_12)
+;
+
+//Funcctions
+
+
+#endif // !__Mushu__
---
+++ Release/Source1.cpp
@@ -0,0 +1,24 @@
+#include"sourse.h"
+
+int testFunction(int X, double Y, char Z)
+
+int main()
+{
+	
+	
+	
+
+	return 0
+}
+
+int testFunction(int X, double Y, char Z)
+{
+	if (Z = "q")
+}
+	else
+{
+	printf("error 2113: not is program")
+}	
+	
+	return X + 5
+}
